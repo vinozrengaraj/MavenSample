ac2d763037d08f1b6735cbd40a99fd2ba895f92c
vinozrengaraj <vinozr@gmail.com>
2021-10-14 12:36:10 +0530

    commit1

---
+++ src/test/java/TestMvnSample.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestMvnSample {
+	public static void main(String[] args) {
+		List<Integer> li=new ArrayList<>();
+		li.add(55);
+		li.add(65);
+		li.add(78);
+		li.add(78);
+		li.add(56);
+		li.add(67);
+		li.add(34);
+		
+	}
+
+}
