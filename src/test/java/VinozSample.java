b4eceee7474486392d874ee91c2a01d9131c0e2f
vinozrangaraj <vinozr@gmail.com>
2021-10-14 11:38:34 +0530

    Vinoz commit

---
+++ src/test/java/VinozSample.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class VinozSample {
+	public static void main(String[] args) {
+		List<Integer> li=new ArrayList<>();
+		li.add(55);
+		li.add(65);
+		li.add(55);
+		li.add(89);
+	
+		
+	}
+}
