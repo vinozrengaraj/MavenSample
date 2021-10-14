4495ca06d8688be69aaff9ce1bc629547e9dfcf8
vinozrangaraj <vinozr@gmail.com>
2021-10-14 11:11:23 +0530

    Sample Commit

---
+++ src/test/java/SampleMvnClass.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleMvnClass {
+  public static void main(String[] args) {
+	List<Integer> li=new ArrayList<>();
+	li.add(55);
+	li.add(65);
+	
+}
+}
