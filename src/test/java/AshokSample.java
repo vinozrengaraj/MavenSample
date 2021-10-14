7302cf8802f9a4e25bad6d0174ad648f7f94d4b2
vinozrangaraj <vinozr@gmail.com>
2021-10-14 11:35:22 +0530

    Ashok commit

---
+++ src/test/java/AshokSample.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AshokSample {
+	 public static void main(String[] args) {
+			List<Integer> li=new ArrayList<>();
+			li.add(55);
+			li.add(65);
+			li.add(55);
+			li.add(89);
+			li.add(34);
+			li.add(44);
+			li.add(45);
+			li.add(78);
+			
+		}
+}
